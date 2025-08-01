9430a70c5c33f6b22dd68617dbe1c77a8945495a
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:40:36 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,1 +1,21 @@
+import React, { useMemo } from 'react';
+
+function UseMemoComponent({ todos }) {
+  const longTodos = useMemo(() => {
+    return todos.filter(todo => todo.length > 5);
+  }, [todos]);
+
+  return (
+    <div className="use-memo">
+      <h2>Todos with more than 5 characters:</h2>
+      <ul>
+        {longTodos.map((todo, i) => (
+          <li key={i}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UseMemoComponent;
 
