592d4b80ced718cae1c4f08fb7e2f2e8c9b11f73
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:41:36 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+const ReactMemoComponent = React.memo(({ todos, onDelete }) => {
+  return (
+    <div className="react-memo">
+      <h2>All Todos:</h2>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={i}>
+            {todo}
+            <button onClick={() => onDelete(i)} className="delete-btn">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
+
+export default ReactMemoComponent;
 
